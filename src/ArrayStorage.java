dbf12aed11cf1fd84b31ee09edffdc4ee17ea984
Viktor <viktor.osheiko@gmail.com>
2023-04-23 13:59:33 +0400

    realize without variable length

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Array based storage for Resumes
  */
@@ -8,20 +12,53 @@ public class ArrayStorage {
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) continue;
+            storage[i] = r;
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        int index = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                index = i;
+                break;
+            }
+        }
+        return storage[index];
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (!storage[i].uuid.equals(uuid)) {
+                continue;
+            } else {
+                if (storage[i + 1] == null || i + 1 == storage.length) {
+                    storage[i] = null;
+                    break;
+                } else {
+                    for (int j = i; j < storage.length-1; j++) {
+                        if (storage[j+1] == null) break;
+                        storage[j] = storage[j + 1];
+                    }
+                }
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int length = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null){
+                length = i;
+                break;
+            }
+        }
+        return Arrays.copyOfRange(storage,0, length);
     }
 
     int size() {
